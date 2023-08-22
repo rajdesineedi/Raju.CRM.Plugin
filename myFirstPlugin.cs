6cf69e460ef5fccda6ecba5d976d891773608f5f
Raju Desineedi <rajud247@outlook.com>
2023-08-22 15:25:51 +0530

    Enter

---
+++ myFirstPlugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace Raju.CRM.Plugin
+{
+    public class myFirstPlugin : IPlugin //Interface to create Plugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            if (context.MessageName.ToLower() == "update" && context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity contact = (Entity)context.InputParameters["Target"];
+                try
+                {
+
+                    if (contact.LogicalName == "contact")
+                    {
+                        tracingService.Trace("Entered into Contact Entity");
+
+                        string lastName = contact.GetAttributeValue<string>("lastname");
+                        string upperCaseLastName = lastName.ToUpper();
+                        contact["lastname"] = upperCaseLastName;
+                        service.Update(contact);
+
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ mySecondPlugin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace Raju.CRM.Plugin
+{
+   public class mySecondPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            if(context.MessageName.ToLower() == "create" && context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                try
+                {
+                    tracingService.Trace("Entered in try block");
+
+                    Entity contact = (Entity)context.InputParameters["Target"];
+                    string emailID = contact.GetAttributeValue<string>("emailaddress1");
+
+                    QueryExpression query = new QueryExpression();
+                    query.ColumnSet = new ColumnSet("emailaddress1");
+                    query.Criteria.AddCondition("emailaddress1", ConditionOperator.Equal, emailID);
+                    EntityCollection collection = service.RetrieveMultiple(query);
+
+                    if(collection.Entities.Count >= 1)
+                    {
+                        throw new InvalidPluginExecutionException("Duplicates values has been found");
+                    }
+
+
+
+                }
+                catch (Exception)
+                {
+
+                    throw new InvalidPluginExecutionException("Invalide Plugin Execution");
+                }
+            }
+
+        }
+    }
+}
